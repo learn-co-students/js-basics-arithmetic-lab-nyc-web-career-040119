f12dd8c2478c75da3095a51fdff28e27e67c896f
OctopiWithHats <sdgranoff@gmail.com>
2019-05-11 23:49:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here
+
+let var1 = 31;
+let var2 = 2;
+let multiply = var1 * var2;
+
+let random = Math.floor(Math.random()) + 1;
+
+let mod = 9 % 5;
+
+let max = (1, 4, 7, 9, 20);
